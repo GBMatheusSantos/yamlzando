3618c121e7213094db8643cf267630c036868afd
Matheus Santos <matheus.henrique@picpay.com>
2022-08-26 00:10:59 -0300

    minor changes

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 import yaml
 import pandas as pd
 
+# nome dos arquivos .yaml que devem ser transformados em csv
 file_list= ["opb_analytics_overdraft", "opb_analytics_transactions"]
+
+# estrutura de dict_list que deve ser considerado
 get_dict = "columns_description"
 
 for file in file_list:
-    with open(f"/Users/matheus.henrique/repos/teste/yaml/{file}.yml") as f:
+    with open(f"yamls/{file}.yml") as f:
         x = yaml.load(f, Loader=yaml.FullLoader)
         pd.DataFrame(x[get_dict]).to_csv(f"{file}.csv")
